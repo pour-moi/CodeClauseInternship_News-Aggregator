943229f50824def644c8e2e8e9a4915fc292627e
pour-moi <137487367+pour-moi@users.noreply.github.com>
2024-02-12 16:25:10 +0300

    divided the new section to another comp

--- News-Aggregator/src/components/News.jsx
+++ News-Aggregator/src/components/News.jsx
@@ -3,14 +3,17 @@ import styled from "styled-components";
 const StyledNews = styled.div`
   img {
     width: 13em;
-    height: 10em;
+    height: 12em;
   }
   display: flex;
+  align-items: center;
+  //   flex-direction: column;
 `;
 
 const Infos = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   width: 10em;
 `;
 
@@ -20,32 +23,14 @@ const DetailInfos = styled.div`
   gap: 1em;
 `;
 
-const StyledRnews = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-function RightSideNews() {
-  return (
-    <>
-      <StyledRnews>
-        <h3>Russia war in Ukraine</h3>
-        <img src="" alt="" />
-        <Infos>
-          <h4>Title</h4>
-          <p>Description</p>
-        </Infos>
-      </StyledRnews>
-    </>
-  );
-}
-
 export function News() {
   return (
     <>
-      <h3>Recent News</h3>
       <StyledNews>
-        <img src="" alt="" />
+        <div>
+          <h1>Recent News</h1>
+          <img src="" alt="" />
+        </div>
         <Infos>
           <h4>Title</h4>
           <p>Description</p>
@@ -55,7 +40,6 @@ export function News() {
             <p>Posted</p>
           </DetailInfos>
         </Infos>
-        <RightSideNews />
       </StyledNews>
     </>
   );
