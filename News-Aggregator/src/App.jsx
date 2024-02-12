cfd3c7cc4d81a1186208cec15f7f981a442c120b
pour-moi <137487367+pour-moi@users.noreply.github.com>
2024-02-12 16:25:27 +0300

    new News section added

--- News-Aggregator/src/App.jsx
+++ News-Aggregator/src/App.jsx
@@ -1,14 +1,21 @@
+import styled from "styled-components";
 import { SearchBar } from "./components/Search";
 import { createGlobalStyle } from "styled-components";
 import { NavBar } from "./components/NavBar";
 import { HeaderNews } from "./components/HeaderNews";
 import { News } from "./components/News";
+import { RightSideNews } from "./components/Rnews";
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
   }
 `;
+
+const StyledNewsView = styled.div`
+  display: flex;
+  justify-content: space-around;
+s`;
 
 function App() {
   return (
@@ -17,7 +24,10 @@ function App() {
       <SearchBar />
       <NavBar />
       <HeaderNews />
-      <News />
+      <StyledNewsView>
+        <News />
+        <RightSideNews />
+      </StyledNewsView>
     </>
   );
 }
