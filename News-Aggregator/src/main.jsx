fc13d4f8d022fec15c51919c710390255a8f1346
pour-moi <137487367+pour-moi@users.noreply.github.com>
2024-02-21 13:41:02 +0300

    removed code

--- News-Aggregator/src/main.jsx
+++ News-Aggregator/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import { Politics } from "./components/CategoryNews.jsx";
 import { NewsCategory } from "./components/CategoryNews.jsx";
 
 const router = createBrowserRouter([
