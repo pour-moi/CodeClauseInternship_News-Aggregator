84861b001ece39fc73432ce28ab691fd346af899
pour-moi <137487367+pour-moi@users.noreply.github.com>
2024-02-21 13:29:27 +0300

    path added for each category

--- News-Aggregator/src/main.jsx
+++ News-Aggregator/src/main.jsx
@@ -1,9 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { Politics } from "./components/CategoryNews.jsx";
+import { NewsCategory } from "./components/CategoryNews.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/App",
+    element: <App />,
+  },
+  {
+    path: "/Politics",
+    element: <NewsCategory category="Politics" />,
+  },
+  {
+    path: "/sport",
+    element: <NewsCategory category="Sport" />,
+  },
+  {
+    path: "/business",
+    element: <NewsCategory category="Business" />,
+  },
+  {
+    path: "/entertainment",
+    element: <NewsCategory category="Entertainment" />,
+  },
+  {
+    path: "/technology",
+    element: <NewsCategory category="Technology" />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
