5d4ee1550bd87c64b4b23756c2ae25ae03765523
pour-moi <137487367+pour-moi@users.noreply.github.com>
2024-02-21 15:06:19 +0300

    fonts added

--- News-Aggregator/src/App.jsx
+++ News-Aggregator/src/App.jsx
@@ -10,12 +10,29 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
   }
+  @font-face {
+    font-family: 'RockStar';
+    src: url('../fonts/RockStar-ExtraLight.ttf') format('truetype');
+  }
+  @font-face {
+    font-family: 'Kanit';
+    src: url('../fonts/Kanit-SemiBold.ttf') format('truetype');
+  }
+  @font-face {
+    font-family: 'Tommy';
+    src: url('../fonts/CreatoDisplay-Regular.otf') format('opentype');
+  }
+  @font-face {
+    font-family: 'Akira';
+    src: url('../fonts/Akira Expanded Demo.otf') format('opentype');
+  }
 `;
 
 const StyledNewsView = styled.div`
   display: flex;
-  justify-content: space-around;
-s`;
+  justify-content: space-evenly;
+  font-family: "RockStar";
+`;
 
 function App() {
   return (
