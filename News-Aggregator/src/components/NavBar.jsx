bcbb814e6917437033d8926832050e56efcf97de
pour-moi <137487367+pour-moi@users.noreply.github.com>
2024-02-21 13:30:07 +0300

    Linked categories

--- News-Aggregator/src/components/NavBar.jsx
+++ News-Aggregator/src/components/NavBar.jsx
@@ -1,31 +1,48 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const StyledNavBar = styled.div`
+const StyledNavBar = styled.nav`
   ul {
     list-style-type: none;
     display: flex;
     justify-content: space-around;
     align-items: center;
+    height: 5em;
+    padding: 0;
   }
   h1 {
     margin: 0;
+    font-family: "Akira";
+  }
+  li {
+    cursor: pointer;
   }
 `;
 
 export function NavBar() {
   return (
-    <>
-      <StyledNavBar>
-        <ul>
-          <li>Home</li>
-          <li>US Politics</li>
-          <li>Technology</li>
-          <h1>ETZena</h1>
-          <li>Business</li>
-          <li>Sport</li>
-          <li>Entertainment</li>
-        </ul>
-      </StyledNavBar>
-    </>
+    <StyledNavBar>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Politics">Politics</Link>
+        </li>
+        <li>
+          <Link to="/Technology">Technology</Link>
+        </li>
+        <h1>ET Zena</h1>
+        <li>
+          <Link to="/Business">Business</Link>
+        </li>
+        <li>
+          <Link to="/Sport">Sport</Link>
+        </li>
+        <li>
+          <Link to="/Entertainment">Entertainment</Link>
+        </li>
+      </ul>
+    </StyledNavBar>
   );
 }
