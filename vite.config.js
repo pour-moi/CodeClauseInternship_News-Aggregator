3ce91b8a852645094a6570224b8ed0a32213c35d
pour-moi <137487367+pour-moi@users.noreply.github.com>
2024-03-04 22:50:58 +0300

    deployed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/CodeClauseInternship_News-Aggregator/",
+  // base: "/CodeClauseInternship_News-Aggregator/",
   plugins: [react()],
 });
